d4b0b54762ad5a785d327075ffc4b70953b400c4
Gabriel Baz <gabrielbzzt@gmail.com>
2025-05-09 15:34:13 -0300

    added CreateFileWithContent.java

---
+++ src/main/java/com/bazgab/CreateFileWithContent.java
@@ -0,0 +1,16 @@
+package com.bazgab;
+
+public class CreateFileWithContent {
+
+    public static void CreateFile(String file) {
+
+    }
+
+    public static void WriteContentToFile(String content)  {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/bazgab/FileOperations.java
+++ src/main/java/com/bazgab/FileOperations.java
@@ -1,10 +1,9 @@
 package com.bazgab;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
+
 import java.nio.file.NoSuchFileException;
-import java.util.Scanner;
+
 
 public class FileOperations {
 
